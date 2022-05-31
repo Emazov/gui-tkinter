ab481039fbc9f4134cea7806c460747f1220c1b1
Emir Almazov <99860815+Emazov@users.noreply.github.com>
2022-05-31 18:20:07 +0600

    finilized filters

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog, Canvas
-
-from PIL import ImageTk, Image
+from tkinter import filedialog, messagebox
+from tkinter import *
+from PIL import ImageTk, Image, UnidentifiedImageError
 
 
 class AlatooApp(tk.Tk):
@@ -15,7 +15,7 @@ class AlatooApp(tk.Tk):
 
     def center_window(self):
         w = 400
-        h = 500
+        h = 600
 
         sw = self.winfo_screenwidth()
         sh = self.winfo_screenheight()
@@ -37,10 +37,10 @@ class AlatooApp(tk.Tk):
 class StartPage(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
-        tk.Label(self, text="Photo editor").pack(side="top", fill="x", pady=20)
+        tk.Label(self, text="Photo editor").pack(side="top", fill="x", pady=(10, 0))
         self.logo = Image.open('./img/ala-too_logo.jpeg')
         self.logo = ImageTk.PhotoImage(image=self.logo)
-        tk.Label(self, image=self.logo).pack(pady=(0, 10))
+        tk.Label(self, image=self.logo).pack(pady=(20, 10))
         tk.Button(self, text="Crop image",
                   command=lambda: master.switch_frame(Crop)).pack()
         tk.Button(self, text="Black & White filter",
@@ -53,38 +53,67 @@ class StartPage(tk.Frame):
 class Crop(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        self.new = None
+        self.cropped = None
         self.fln = None
         self.img = None
         self.canvas = Canvas(self, width=350, height=350)
-        tk.Label(self, text="Crop").pack(side="top", pady=10)
+        tk.Label(self, text="Crop Image").pack(side="top", pady=10)
         self.config()
         tk.Button(self, text="Return to start page",
-                  command=lambda: master.switch_frame(StartPage)).pack(side=tk.BOTTOM, pady=10)
+                  command=lambda: master.switch_frame(StartPage)).pack(side=tk.BOTTOM, pady=(10, 20))
 
-    #   ------------    'show_image' and 'config' from YouTube chanel "Python world"   ------------
+    #   ------------    'config' and 'show_image' from YouTube chanel "Python world"   ------------
     def config(self):
         self.canvas.pack()
 
         tk.Button(self, text="Open Image", command=self.show_image).pack()
 
-        # tk.Button(self, text="Crop Image", command=self.crop_image).pack()
+        tk.Button(self, text="Crop Image", command=self.crop_image).pack(pady=10)
+
+        # tk.Button(self, text="Download Image", command=self.download_image).pack()
 
     def show_image(self):
         self.fln = filedialog.askopenfilename()
-        self.img = Image.open(self.fln)
-        self.img.thumbnail((350, 350))
-        self.img = ImageTk.PhotoImage(self.img)
-        self.canvas.create_image(0, 1, image=self.img, anchor='nw')
+        try:
+            self.img = Image.open(self.fln)
+            self.img.thumbnail((350, 350))
+            self.img = ImageTk.PhotoImage(self.img)
+            self.canvas.create_image(0, 1, image=self.img, anchor='nw')
+        except UnidentifiedImageError:
+            messagebox.showerror(
+                "Error", "Image not defined"
+            )
+
+    #   ------------    function 'crop' from github.com/Emazov  ------------
+    def crop_image(self):
+        self.cropped = Image.open(self.fln)
+
+        w = self.cropped.width
+        h = self.cropped.height
+        new_width = 1080
+        new_height = 1080
+
+        left = (w - new_width) / 2
+        top = (h - new_height) / 2
+        right = (w + new_width) / 2
+        bottom = (h + new_height) / 2
+
+        self.cropped = self.cropped.crop((left, top, right, bottom))
+        self.cropped.thumbnail((350, 350))
+        self.cropped = ImageTk.PhotoImage(self.cropped)
+        self.canvas.create_image(1, 1, image=self.cropped, anchor='nw')
 
 
 #   /-------------------------------------/      BLACK & WIGHT - PAGE       /----------------------------/
 class BlackWhite(tk.Frame):
     def __init__(self, master):
         tk.Frame.__init__(self, master)
+        self.new = None
         self.fln = None
         self.img = None
         self.canvas = Canvas(self, width=350, height=350)
-        tk.Label(self, text="BlackWhite").pack(side="top", pady=10)
+        tk.Label(self, text="Black and White filter").pack(side="top", pady=10)
         self.config()
         tk.Button(self, text="Return to start page",
                   command=lambda: master.switch_frame(StartPage)).pack(side=tk.BOTTOM, pady=10)
@@ -95,7 +124,9 @@ class BlackWhite(tk.Frame):
 
         tk.Button(self, text="Open Image", command=self.show_image).pack()
 
-        # tk.Button(self, text="Apply filter", command=self.bw_image).pack()
+        tk.Button(self, text="Use filter", command=self.filter_image).pack(pady=10)
+
+        # tk.Button(self, text="Download Image", command=self.download_image).pack()
 
     def show_image(self):
         self.fln = filedialog.askopenfilename()
@@ -103,6 +134,13 @@ class BlackWhite(tk.Frame):
         self.img.thumbnail((350, 350))
         self.img = ImageTk.PhotoImage(self.img)
         self.canvas.create_image(0, 1, image=self.img, anchor='nw')
+
+    def filter_image(self):
+        self.new = Image.open(self.fln)
+        self.new = self.new.convert(mode='L')
+        self.new.thumbnail((350, 350))
+        self.new = ImageTk.PhotoImage(self.new)
+        self.canvas.create_image(0, 1, image=self.new, anchor='nw')
 
 
 #   /-------------------------------------/      ABOUT-COPYRIGHT - PAGE       /----------------------------/
